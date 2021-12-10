7e8056345c96e59951f86705b7ddd8b5c9ad5197
khoidd <khoidd@outlook.com>
2021-12-10 14:20:34 +0700

    add pick_audio

--- src/util/audio.py
+++ src/util/audio.py
@@ -1,3 +1,5 @@
+import random as rd
+
 import tensorflow as tf
 import tensorflow_io as tfio
 
@@ -20,3 +22,19 @@ def read_mp3(path: str) -> tf.Tensor:
 def read_wav(path: str) -> tf.Tensor:
     audio = tfio.audio.decode_wav(tf.io.read_file(path))
     return audio
+
+
+def pick_audio(data: tf.Tensor, length: int) -> tf.Tensor:
+    size = data.shape[0] - length
+    if size < 0:
+        size = data.shape[0] // 2
+    start = rd.randint(0, size)
+    output = data[start: start + length, :]
+
+    len_out = output.shape[0]
+    print(start)
+    print(len_out)
+    if len_out < length:
+        paddings = tf.constant([[0, length - len_out], [0, 0]])
+        output = tf.pad(output, paddings, "CONSTANT")
+    return output
